08a9e6fe28a60f5a2b8f6be79af0b60ee92f0a48
“Oksana <oksanaoprysk01@gmail.com>
2026-01-19 00:24:57 +0200

    add login page component and locator

---
+++ locators/LoginPageLocators.ts
@@ -0,0 +1,17 @@
+import { Locator } from "@playwright/test";
+import { BaseLocators } from "./BaseLocators";
+
+export class LoginPageLocators extends BaseLocators {
+  readonly emailInputLocator: Locator = this.baseLocator.getByRole("textbox", {
+    name: "Email",
+  });
+  readonly passwordInputLocator: Locator = this.baseLocator.getByRole(
+    "textbox",
+    {
+      name: "Password",
+    }
+  );
+  readonly signInButtonLocator: Locator = this.baseLocator.getByRole("button", {
+    name: "Sign in",
+  });
+}
---
+++ pages/LoginPage.ts
@@ -0,0 +1,27 @@
+import BasePage from "./BasePage";
+import { LoginPageLocators } from "../locators/LoginPageLocators";
+
+class LoginPage extends BasePage {
+  readonly locators: LoginPageLocators = new LoginPageLocators(
+    this.page.locator("body")
+  );
+
+  async fillEmail(email: string) {
+    await this.locators.emailInputLocator.fill(email);
+  }
+  async fillPassword(password: string) {
+    await this.locators.passwordInputLocator.fill(password);
+  }
+  async clickSignInButton() {
+    await this.locators.signInButtonLocator.click();
+  }
+
+  async userLogin(email: string, password: string) {
+    await this.navigateToPage();
+    await this.fillEmail(email);
+    await this.fillPassword(password);
+    await this.clickSignInButton();
+  }
+}
+
+export default LoginPage;
