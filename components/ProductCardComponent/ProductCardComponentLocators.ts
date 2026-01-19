48fac6d59634f313d6049486ac339bfa9d03ff88
“Oksana <oksanaoprysk01@gmail.com>
2026-01-19 10:54:46 +0200

    delete comment

--- components/ProductCardComponent/ProductCardComponentLocators.ts
+++ components/ProductCardComponent/ProductCardComponentLocators.ts
@@ -2,19 +2,6 @@ import { BaseLocators } from "@/locators/BaseLocators";
 import { Locator } from "@playwright/test";
 
 export class ProductCardLocators extends BaseLocators {
-  // readonly productCard: Locator = this.page
-  //   .getByRole("article")
-  //   .filter({ hasText: " Quick view The best is yet" });
-  // readonly favoriteProductButtonLocator: Locator = this.page.getByRole(
-  //   "button",
-  //   {
-  //     name: "favorite_border",
-  //   }
-  // );
-  // readonly cardDescriptionLocator: Locator = this.page.getByRole("link", {
-  //   name: "Mug The Best Is Yet To Come",
-  // });
-
   favoriteProductButtonLocator = this.baseLocator.getByRole("button", {
     name: "favorite_border",
   });
