73288d7a18c49b51ee781a35c9d5d52f03820c70
“Oksana <oksanaoprysk01@gmail.com>
2026-02-08 18:12:55 +0200

    fix header

--- components/HeaderComponent/HeaderComponent.ts
+++ components/HeaderComponent/HeaderComponent.ts
@@ -1,14 +1,8 @@
 import { BaseComponent } from '../BaseComponent';
 import { HeaderLocators } from './HeaderComponentLocators';
-import { Locator } from '@playwright/test';
 
 export class HeaderComponent extends BaseComponent {
-  readonly locators: HeaderLocators;
-
-  constructor(baseLocator: Locator) {
-    super(baseLocator);
-    this.locators = new HeaderLocators(this.baseLocator);
-  }
+  locators: HeaderLocators = new HeaderLocators(this.baseLocator);
 
   async signIn() {
     await this.locators.signInLocator.click();
